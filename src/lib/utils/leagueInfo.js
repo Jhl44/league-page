801ab75fd454e4eb5d3e9b129ab49df60d67a408
Jhl44 <121775783+Jhl44@users.noreply.github.com>
2024-06-30 23:17:44 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -250,7 +250,7 @@ export const managers = [
     },
     {
       "roster": 11,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-      "managerID" : 941784831560712192
+      "managerID" : 941784831560712192,
       "name": "Katie LeFort",
 //       "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "San Diego, CA", // (optional)
@@ -293,7 +293,7 @@ export const managers = [
 //       "philosophy": "Trade central",
       "tradingScale": 50, // 1 - 10
       "preferredContact": "Text", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
-    }
+    },
   ]
   
   
