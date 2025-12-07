51ef1e53af8e3f55a052901831a829835fbb3edf
Jhl44 <121775783+Jhl44@users.noreply.github.com>
2025-12-07 17:22:41 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "1053852882170241024"; // your league ID
+export const leagueID = "1197746138573185024"; // your league ID
 export const leagueName = "Dynasty Country, Let's Ride"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
